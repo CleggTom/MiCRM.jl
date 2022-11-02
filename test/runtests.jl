1b09000b7b5b012eb6bd1ef11d4ddaf4968d43e9
Tom <t.clegg17@imperial.ac.uk>
2022-11-02 13:03:18 +0000

    Adding basic test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,2 +1,11 @@
 using MiCRM
 using Test
+
+@time begin
+    using MiCRM
+    p = MiCRM.Parameters.generate_params(10,10,λ = 0.3)
+    u0 = ones(20)
+    t_span = (0.0, 100.0)
+    prob = ODEProblem(MiCRM.Simulations.dx!, u0, t_span, p)
+    sol = solve(prob, AutoTsit5(Rosenbrock23()))
+end
